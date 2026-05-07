453e650ce7af1485d455c5f1f7e89182528e2240
Craig D'Silva <craig.dsilva10@gmail.com>
2026-05-07 15:08:41 +0100

    Fetch quotes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,33 @@
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-    <></>
-  )
-}
+  const [quote, setQuote] = useState();
 
-export default App
+  const fetchQuote = useCallback(async () => {
+    try {
+      const res = await fetch("http://localhost:3000/random");
+      const data = await res.json();
+      setQuote(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchQuote();
+  }, [fetchQuote]);
+
+  return (
+    <>
+      {quote && (
+        <p className="quote">
+          <q className="quote-text">{quote.quote}</q>
+          <em className="quote-author">{`- ${quote.author}`}</em>
+        </p>
+      )}
+    </>
+  );
+};
+
+export default App;
